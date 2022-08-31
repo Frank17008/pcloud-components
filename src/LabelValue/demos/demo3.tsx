ee03e9b1b6dd8e5c66ead3b1c982e46f9eeb0e19
donglei <405267330@qq.com>
2022-08-31 17:57:05 +0800

    示例修改

--- src/LabelValue/demos/demo3.tsx
+++ src/LabelValue/demos/demo3.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import LabelValue from '../index';
+import { LabelValue } from 'pui-components';
 
 export default () => <LabelValue label="姓名" value="" emptyValue="--" />;
