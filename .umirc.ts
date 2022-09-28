dbbc889f7b9e65f55579dc75f9d9c355a2481553
donglei <405267330@qq.com>
2022-09-28 16:14:33 +0800

    更换logo

--- .umirc.ts
+++ .umirc.ts
@@ -3,9 +3,8 @@ const path = require('path');
 
 export default defineConfig({
   title: 'pui-components',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/images/logo.png',
+  logo: '/images/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   lessLoader: { javascriptEnabled: true },
