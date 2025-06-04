927cbbe08d2d65df4e764c6e097dcaccd5c70ff3
Frank17008 <405267330@qq.com>
2025-06-04 16:42:41 +0800

    docs(dumi): update site config and add theme settings

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,7 +5,19 @@ export default defineConfig({
   resolve: { docDirs: ['docs'] },
   outputPath: 'docs-dist',
   favicons: ['/images/favicon.png'],
-  logo: '/images/favicon.png',
+  title: 'pcloud-components组件库',
+  themeConfig: {
+    name: 'pcloud-components',
+    logo: '/images/favicon.png',
+    editLink: true,
+    footer:
+      '<p>© 2023-present <a href="http://www.pcgi.com.cn" target="_blank">PointCloud</a>. All rights reserved.</p><p>Powered by <a href="https://d.umijs.org" target="_blank">Dumi</a>.</p><p>Author: Frank Dong</p>',
+    socialLinks: {
+      github: 'https://github.com',
+      yuque: 'https://www.yuque.com',
+      zhihu: 'https://www.zhihu.com',
+    },
+  },
   locales: [
     { id: 'zh-CN', name: '中文', suffix: '' },
     { id: 'en-US', name: 'English', suffix: '-en' },
