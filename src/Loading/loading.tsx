8f258358afdf5aed65560a08e455086cf12ef194
Frank17008 <405267330@qq.com>
2025-06-05 17:32:05 +0800

    feat(Loading): add delay prop for loading effect

--- src/Loading/loading.tsx
+++ src/Loading/loading.tsx
@@ -1,16 +1,17 @@
-import React, { useContext, useEffect } from 'react';
-import ReactDOM from 'react-dom';
 import { Spin } from 'antd';
 import classNames from 'classnames';
+import { useContext, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { ConfigContext } from '../ConfigProvider';
 import { LoadingInstanceProps } from './interface';
 import './styles/index.less';
 
 function Loading(props: LoadingInstanceProps) {
-  const { tip = '数据请求中...' } = props;
+  const { delay, ...other } = props;
   const { prefixCls, getPrefixCls }: any = useContext(ConfigContext);
   const classname = getPrefixCls('loading');
   const wrapperClass = classNames({ [`${prefixCls}-loading`]: !!prefixCls }, classname);
+  const [visible, setVisible] = useState<boolean>(false);
 
   useEffect(() => {
     document.body.style.overflow = 'hidden';
@@ -18,11 +19,21 @@ function Loading(props: LoadingInstanceProps) {
       document.body.style.overflow = '';
     };
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setVisible(true), delay || 0);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [delay]);
+  if (!visible) return <></>;
   return (
     <div className={wrapperClass}>
       <div className="mask" />
       <div className="loading">
-        <Spin tip={tip} />
+        <Spin {...other}>
+          <div style={{ opacity: 0 }}>{other.tip}</div>
+        </Spin>
       </div>
     </div>
   );
@@ -30,6 +41,7 @@ function Loading(props: LoadingInstanceProps) {
 
 Loading.newInstance = function newNotificationInstance(args: LoadingInstanceProps) {
   const { container, ...otherProps } = args || {};
+
   const div = document.createElement('div');
   // eslint-disable-next-line react/no-find-dom-node
   const element: any = ReactDOM.findDOMNode(container);
