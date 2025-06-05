3c4af237e172e1f3c1080a70c1e00ffefab0079c
Frank17008 <405267330@qq.com>
2025-06-05 17:31:10 +0800

    refactor(Loading): update types and add delay prop
    - Add `delay` prop to `LoadingInstanceProps` interface
    - Update `open` and `getInstance` methods to return `React.ReactNode`
    - Remove `SpinProps`'s `delay` prop from `LoadingInstanceProps`

--- src/Loading/interface.ts
+++ src/Loading/interface.ts
@@ -1,19 +1,24 @@
 import { SpinProps } from 'antd';
 import React, { ReactInstance } from 'react';
 
-export interface LoadingInstanceProps extends SpinProps {
+export interface LoadingInstanceProps extends Omit<SpinProps, 'delay'> {
   /**
    * @description 加载框容器
    * @default body
    */
 
   container?: ReactInstance | undefined;
+  /**
+   * @description 延迟显示加载框毫秒
+   * @default 0
+   */
+  delay?: number;
 }
 
 export interface ILoadingProps {
-  open: (param: LoadingInstanceProps) => React.ReactDOM;
+  open: (_param: LoadingInstanceProps) => React.ReactNode;
   close: () => void;
-  getInstance: () => React.ReactDOM;
+  getInstance: () => React.ReactNode;
 }
 
 export type LoadingProps = ILoadingProps;
